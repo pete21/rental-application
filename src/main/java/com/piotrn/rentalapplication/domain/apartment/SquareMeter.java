85305b7004807a27ca0edea7586dd8dfd112a342
Piotr Nazarewicz <piotr.nazarewicz@symmetrical.ai>
2021-06-30 23:58:28 +0200

    Adding new Apartment - creation of the ApartmentApplicationService

---
+++ src/main/java/com/piotrn/rentalapplication/domain/apartment/SquareMeter.java
@@ -0,0 +1,10 @@
+package com.piotrn.rentalapplication.domain.apartment;
+
+public class SquareMeter {
+    private final Double size;
+
+    public SquareMeter(Double size) {
+
+        this.size = size;
+    }
+}
