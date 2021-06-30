a06a6f55790bc5d97eb1581d0b860afa1365f77a
Piotr Nazarewicz <piotr.nazarewicz@symmetrical.ai>
2021-07-01 00:13:47 +0200

    Adding new Apartment - encapsulate attributes of Apartment and its constructor in the package

--- src/main/java/com/piotrn/rentalapplication/domain/apartment/Room.java
+++ src/main/java/com/piotrn/rentalapplication/domain/apartment/Room.java
@@ -1,11 +1,14 @@
 package com.piotrn.rentalapplication.domain.apartment;
 
-public class Room {
+class Room {
     private final String name;
     private final SquareMeter squareMeter;
 
-    public Room(String name, SquareMeter squareMeter) {
+    Room(String name, SquareMeter squareMeter) {
         this.name = name;
         this.squareMeter = squareMeter;
+    }
+    public void test() {
+        Room r = new Room("a",new SquareMeter(1.0));
     }
 }
