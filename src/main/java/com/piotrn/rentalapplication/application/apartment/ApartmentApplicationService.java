8fcbd5321d8f14c3b669c84e507c1d71e47fa09e
Piotr Nazarewicz <piotr.nazarewicz@symmetrical.ai>
2021-07-22 00:44:26 +0200

    Adding new Apartment - save newly created apartment in ApartmentRepository

--- src/main/java/com/piotrn/rentalapplication/application/apartment/ApartmentApplicationService.java
+++ src/main/java/com/piotrn/rentalapplication/application/apartment/ApartmentApplicationService.java
@@ -2,12 +2,22 @@ package com.piotrn.rentalapplication.application.apartment;
 
 import com.piotrn.rentalapplication.domain.apartment.Apartment;
 import com.piotrn.rentalapplication.domain.apartment.ApartmentFactory;
+import com.piotrn.rentalapplication.domain.apartment.ApartmentRepository;
 
 import java.util.Map;
 
 public class ApartmentApplicationService {
+
+    private final ApartmentRepository apartmentRepository;
+
+    public ApartmentApplicationService(ApartmentRepository apartmentRepository) {
+        this.apartmentRepository = apartmentRepository;
+    }
+
     public void add(String ownerId, String street, String postalCode, String houseNumber, String apartmentNumber, String city, String country, String description, Map<String, Double> roomsDefinition) {
 
         Apartment apartment = new ApartmentFactory().create(ownerId, street, postalCode, houseNumber, apartmentNumber, city, country, description, roomsDefinition);
+
+        apartmentRepository.save(apartment);
     }
 }
---
+++ src/main/java/com/piotrn/rentalapplication/domain/apartment/ApartmentRepository.java
@@ -0,0 +1,5 @@
+package com.piotrn.rentalapplication.domain.apartment;
+
+public interface ApartmentRepository {
+    void save(Apartment apartment);
+}
